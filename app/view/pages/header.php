00bd68307553675cd54f5e48a443345a79d6dda8
Alexandr Podluzhnyi <sanchoman314@gmail.com>
2021-03-01 13:06:27 +0200

    Add bucket&main pages
    Add some design and fix bugs.

--- app/view/pages/header.php
+++ app/view/pages/header.php
@@ -1,48 +1,77 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height: 100%;">
+
 <head>
     <meta charset="UTF-8">
     <title> Title </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link href="../../../framework/tools/bootstrap-4.5.0/dist/css/bootstrap.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="catalog">GameShop</a>
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
 
-    <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="main"> Home </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="catalog"> Games Catalog </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"> About us </a>
-            </li>
-        </ul>
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="../../public/css/main.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" style="font-size: 25px;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="main" style="font-size: 25px;">AlexGameShop</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="main"> Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="catalog"> Games Catalog </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> About us </a>
+                </li>
+            </ul>
             <?php if (!isset($_SESSION['name'])): ?>
-            <a class="nav-link" href="signin">
-                <button rel="signin" class="btn btn-success">
-                    Sign In
-                </button>
-            </a>
-           <a class="nav-link" href="register">
-                <button rel="register" class="btn btn-success">
-                    Create Account
-                </button>
-            </a>
-
+                <a class="nav-link" href="signin">
+                    <button rel="signin" class="btn btn-lg btn-outline-success">
+                        Sign In
+                    </button>
+                </a>
+                <a class="nav-link" href="register">
+                    <button rel="register" class="btn btn-lg btn-outline-info">
+                        Create Account
+                    </button>
+                </a>
             <?php else: ?>
-                <ul class="nav-item" >
-                    <a class="nav-item" href="member"> Hi, <?php echo $_SESSION['name']?> </a>
-                </ul>
+                <a class="nav-link" href="bucket">
+                    <button rel="signin" class="btn btn-lg btn-outline-info">
+                        Bucket
+                    </button>
+                </a>
+                    <a class="nav-item" href="member">
+                        <button rel="member" class="btn btn-lg btn-outline-warning">
+                            Hi, <?php echo $_SESSION['name']?>
+                        </button>
+                    </a>
             <?php endif; ?>
+        </div>
     </div>
 </nav>
+
+</head>
--- app/view/pages/templates/catalogTpl.php
+++ app/view/pages/templates/catalogTpl.php
@@ -1,10 +1,10 @@
-
-<?php if ($products['session']->keyExists('cart_list') && count($products['session']->get('cart_list')) > 0)
-    {
-        $countProducts = count($products['session']->get('cart_list'));
-    }
+<?php
+    if ($products['session']->keyExists('cart_list') && count($products['session']->get('cart_list')) > 0)
+        {
+            $countProducts = count($products['session']->get('cart_list'));
+        }
+    foreach ($products['products'] as $item):
 ?>
-<?php foreach ($products['products'] as $item): ?>
     <div class="card">
         <img src= "<?php echo $item->image?>" alt="...">
         <div class="card-body">
@@ -21,12 +21,19 @@
             ?>
             </small></p>
             <div class="text text-right">
-                <h3><b><?php echo $item->price . "$"?></b></h3>
+                <h3><b><?php echo $item->price . "$";
+                ?></b></h3>
                 <button href="#" class="btn btn-primary"> View </button>
                 <button href="#" class="btn btn-success"> Buy </button>
+
+                <form action="catalog/addProduct" method="post" class="row" style="width: 100%">
+                    <button class="btn btn-primary" name="addToBucketBtn" value="<?php echo $item->id ?>">
+                        В корзину
+                    </button>
+                </form>
+                <?php echo $item->id ?>
             </div>
         </div>
     </div>
     <br>
 <?php endforeach; ?>
-
